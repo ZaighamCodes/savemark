f2e11c35267d22a30843e9066ff83aba2fae3737
ZaighamCodes <mdzaigham27@gmail.com>
2026-09-15 10:40:32 +0530

    (feat) created login and register page with register api integrated

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'home',
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '../../services/auth';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'register',
-  imports: [RouterLink],
+  imports: [ FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+  username = '';
+  password = '';
+  confirmPassword = '';
+  private auth = inject(Auth);
+  private router=inject(Router)
+  registerUser(form: NgForm) {
+    if (!this.username || !this.password) {
+      alert('All fields are mandatory');
+      return;
+    }
+    if (this.password !== this.confirmPassword) {
+      alert('Passwords do not match');
+      this.password = '';
+      this.confirmPassword = '';
+      return;
+    }
+    if (this.username.length < 5) {
+      alert('Username should be at least 5 characters long');
+      
+      return;
+    }
+    if (this.password.length < 8) {
+      alert('Password should be at least 8 characters long');
+      this.password = '';
+      this.confirmPassword = '';
+      return;
+    }
+
+    this.auth.registerUser({ username: this.username, password: this.password }).subscribe({
+      next: (response) => {
+        console.log('response from server', response);
+
+        alert('user registered successfully');
+        this.router.navigate(['/login']);
+        // form.resetForm();
+      },
+      error: (error) => {
+        console.log('error from server', error);
+        alert('Something went wrong');
+      },
+    });
+  }
+}
